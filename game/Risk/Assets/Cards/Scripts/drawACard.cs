3100937883e60d67d86418af4985c163ad59a1ed
Alex Scheiner <derbymutt@gmail.com>
2017-04-06 16:03:48 -0400

    New Board Functionality
    Adds functionality for board

---
+++ game/Risk/Assets/Cards/Scripts/drawACard.cs
@@ -0,0 +1,271 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class drawACard : MonoBehaviour {
+
+    // Use this for initialization
+    bool big = false;
+    bool centeringX = false;
+    bool centeringY = false;
+    bool returningX = false;
+    bool returningY = false;
+    bool returningScaleX = false;
+    bool returningScaleY = false;
+    bool greaterX = false;
+    bool greaterY = false;
+    bool lesserX = false;
+    bool lesserY = false;
+    bool rotating = false;
+    bool sideChoice = false;
+    bool changeFace = false;
+    bool shrinking = false;
+    bool doingSomething = false;
+    bool returningOrCentering = true;
+    bool wasShrinking = false;
+    readonly float cardWidth = 1000;
+    readonly float cardHeight = 1400;
+    readonly float bigRatio = .7f;
+    readonly float smallRatio = .1f;
+    readonly float enlargeScale = .03f;
+    readonly float centerXScale = .2f;
+    readonly float centerYScale = .095f;
+    readonly float rotateSpeed = 3f;
+    float xOrigin;
+    float yOrigin;
+    float rotationCount = 0;
+
+    Vector3 originalScale;
+
+    bool enlarging = false;
+    float screenRatio;
+	void Start () {
+        xOrigin = transform.position.x;
+        yOrigin = transform.position.y;
+        
+        screenRatio = (Screen.width * smallRatio);
+        Vector3 theScale = new Vector3(screenRatio/cardHeight, screenRatio/cardHeight,0f);
+        transform.localScale = theScale;
+        originalScale = transform.localScale;
+        
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        screenRatio = (Screen.width * smallRatio);
+
+        if (enlarging)
+        {
+            Vector3 theScale = new Vector3(transform.localScale.x + enlargeScale, transform.localScale.y + enlargeScale);
+            transform.localScale = theScale;
+
+            if(transform.localScale.x >= bigRatio)
+            {
+
+                theScale = new Vector3(bigRatio, bigRatio);
+                transform.localScale = theScale;
+
+                enlarging = false;
+            }
+        }
+
+        if(shrinking)
+        {
+            Vector3 theScale = new Vector3(transform.localScale.x - enlargeScale, transform.localScale.y - enlargeScale);
+            transform.localScale = theScale;
+
+            if (transform.localScale.x <= originalScale.x)
+            {
+
+                theScale = new Vector3(originalScale.x, originalScale.x);
+                transform.localScale = theScale;
+
+                enlarging = shrinking = false;
+            }
+
+        }
+
+        //center the x position
+        if (centeringX)
+        {
+            Vector3 thePosition;
+            if (greaterX) {
+                thePosition = new Vector3(transform.position.x - centerXScale, transform.position.y, transform.position.z);
+
+                if (transform.position.x <= Camera.main.transform.position.x)
+                {
+                    thePosition = new Vector3(Camera.main.transform.position.x, transform.position.y, transform.position.z);
+                    centeringX = false;
+                }
+                    }
+            else
+            {
+                thePosition = new Vector3(transform.position.x + centerXScale, transform.position.y, transform.position.z);
+                if (transform.position.x >= Camera.main.transform.position.x)
+                {
+                    thePosition = new Vector3(Camera.main.transform.position.x, transform.position.y, transform.position.z);
+                    centeringX = false;
+                }
+            }
+            transform.position = thePosition;
+
+        }
+
+        //center the y position
+        if (centeringY)
+        {
+            Vector3 thePosition;
+            if (greaterY)
+            {
+                thePosition = new Vector3(transform.position.x, transform.position.y - centerYScale, transform.position.z);
+
+                if (transform.position.y <= Camera.main.transform.position.y)
+                {
+                    thePosition = new Vector3(transform.position.x, Camera.main.transform.position.y, transform.position.z);
+                    centeringY = false;
+                }
+            }
+            else
+            {
+                thePosition = new Vector3(transform.position.x, transform.position.y + centerYScale, transform.position.z);
+                if (transform.position.y >= Camera.main.transform.position.y)
+                {
+                    thePosition = new Vector3(transform.position.x, Camera.main.transform.position.y, transform.position.z);
+                    centeringY = false;
+                }
+            }
+            transform.position = thePosition;
+
+        }
+
+
+        //returning
+        if (returningX)
+        {
+
+            //returning x
+            Vector3 thePosition;
+            if (lesserX)
+            {
+                thePosition = new Vector3(transform.position.x + centerXScale, transform.position.y, transform.position.z);
+
+                if (transform.position.x >= xOrigin)
+                {
+                    thePosition = new Vector3(xOrigin, transform.position.y, transform.position.z);
+                    returningX = false;
+                }
+            }
+            else
+            {
+                thePosition = new Vector3(transform.position.x - centerXScale, transform.position.y, transform.position.z);
+                if (transform.position.x <= xOrigin)
+                {
+                    thePosition = new Vector3(xOrigin, transform.position.y, transform.position.z);
+                    returningX = false;
+
+                }
+            }
+            transform.position = thePosition;
+        }
+
+        if (returningY)
+        {
+
+            //returning y
+            Vector3 thePosition;
+            if (lesserY)
+            {
+                thePosition = new Vector3(transform.position.x, transform.position.y + centerYScale, transform.position.z);
+
+                if (transform.position.y >= yOrigin)
+                {
+                    thePosition = new Vector3(transform.position.x, yOrigin, transform.position.z);
+                    returningY = false;
+                }
+            }
+            else
+            {
+                thePosition = new Vector3(transform.position.x, transform.position.y -centerYScale, transform.position.z);
+                if (transform.position.y <= yOrigin)
+                {
+                    thePosition = new Vector3(transform.position.y, yOrigin, transform.position.z);
+                    returningY = false;
+
+                }
+            }
+            transform.position = thePosition;
+        }
+
+
+
+        if (!centeringX && !centeringY && !enlarging && !rotating && !returningX && !returningY &&!shrinking)
+        {
+            
+            if (doingSomething)
+            {
+                doingSomething = false;
+                //transform.Find("Return").GetComponent<BoxCollider2D>().enabled = true;
+                GetComponent<BoxCollider2D>().enabled = true;
+            }
+
+            if (wasShrinking)
+            {
+                wasShrinking = false;
+                //transform.GetComponent<BoxCollider2D>().enabled = true;
+            }
+        }
+
+        
+
+
+    }
+
+     void enlarge()
+    {
+        //zoom
+        enlarging = true;
+        
+    }
+
+    public void shrink()
+    {
+        //zoom out
+        wasShrinking = true;
+        shrinking = true;
+        returningX = true;
+        lesserX = transform.position.x < xOrigin;
+        returningY = true;
+        lesserY = transform.position.y < yOrigin;
+        
+    }
+
+    void centerX()
+    {
+        centeringX = true;
+        greaterX = transform.position.x > Camera.main.transform.position.x;
+    }
+
+    void centerY()
+    {
+        centeringY = true;
+        greaterY = transform.position.y > Camera.main.transform.position.y;
+    }
+
+    void rotate()
+    {
+        rotating = true;
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            doingSomething = true;
+                enlarge();
+                centerX();
+                centerY();         
+             
+        }
+    }
+}
---
+++ game/Risk/Assets/CountryHandler.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CountryHandler : MonoBehaviour {
+
+	public Color[] colors;
+	public Transform Spawnable;
+	public Transform Parent;
+	public Transform[] Spawned;
+	private int numSpawned =1023;
+	public float Distance = 12f;
+	public Dropdown selecter;
+	public Dropdown TroopSelecter;
+	public int selected = -1;
+	public Texture[] images;
+
+	// Use this for initialization
+	void Start () {
+		Spawned = new Transform[1023];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (1)) {
+			if (isClicked () == -1) {
+				int temp = 0;
+				while (Spawned [temp] != null)
+					temp++;
+				Spawned [temp] = Instantiate (Spawnable).transform;
+				Spawned [temp].transform.position = FindObjectOfType<Camera> ().ScreenPointToRay (Input.mousePosition).origin;
+				Spawned [temp].transform.position = new Vector3 (Spawned [temp].position.x, 0f -  Spawned [temp].position.z / 500f, Spawned [temp].position.z);
+				Spawned [temp].transform.parent = Parent;
+				Spawned [temp].GetComponent<MeshRenderer> ().material.color = colors [selecter.value];
+				Spawned [temp].GetComponent<MeshRenderer> ().material.mainTexture = images [TroopSelecter.value];
+			} else {
+				Destroy (Spawned [isClicked()].gameObject);
+			}
+		}
+
+		if (Input.GetMouseButton (0)) {
+			if (isClicked () != -1 || selected != -1) {
+				if (selected == -1) {
+					selected = isClicked ();
+				}
+				Spawned [selected].transform.position = FindObjectOfType<Camera> ().ScreenPointToRay (Input.mousePosition).origin;
+				Spawned [selected].transform.position = new Vector3 (Spawned [selected].position.x, 0f -  Spawned [selected].position.z / 500f, Spawned [selected].position.z);
+			}
+		}
+		if (Input.GetMouseButtonUp (0)) {
+			selected = -1;
+		}
+	}
+
+	public int isClicked() {
+		for (int i = 0; i < numSpawned; i++) {
+			if (Spawned [i] != null) {
+				float xoff = Mathf.Abs (Spawned [i].position.x - FindObjectOfType<Camera> ().ScreenPointToRay (Input.mousePosition).origin.x);
+				float yoff = Mathf.Abs (Spawned [i].position.z - FindObjectOfType<Camera> ().ScreenPointToRay (Input.mousePosition).origin.z);
+				float dist = Mathf.Sqrt (xoff * xoff + yoff * yoff);
+				if (dist < Distance)
+					return i;
+			}
+		}
+		return -1;
+	}
+
+	public void clearAllObjects() {
+		for(int i = 0; i<numSpawned; i++)
+			if(Spawned[i] != null)
+				Destroy (Spawned [i].gameObject);
+	}
+	public Color GetColor(int i) {
+		return colors [i];
+	}
+}
